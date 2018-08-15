2e60b73e70503cbdb06915fd8f4abba543723dd5
Jola <recka.jolanta@gmail.com>
2018-08-15 19:28:42 +0200

    personal data and parents data added

--- src/components/IdForm.js
+++ src/components/IdForm.js
@@ -40,7 +40,7 @@ class IdForm extends React.Component{
                 <div className='row'>
                 <form className='col-12'>
                         <div className='form-group'>
-                        <div className="caption">Personal data</div>
+                        <div className='caption'>Personal data</div>
                             <label>
                                 Social security number
                             </label>
@@ -48,11 +48,136 @@ class IdForm extends React.Component{
                                 className='form-control'
                                 type='text'
                                 onChange={this.handleChange}
-                                name='socialSecurityNumber'>
+                                name='socialSecurityNumber'
+                                value={this.state.socialSecurityNumber}>
                             </input>       
                         </div>
-                        </form>
-                    
+                        <div className='form-group'>
+                            <label>
+                                Name
+                            </label>
+                            <input 
+                                className='form-control'
+                                name='name'
+                                type='text'
+                                onChange={this.state.handleChange}
+                                value={this.state.handleChange}/>
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Surname
+                            </label>
+                            <input
+                                name='surname'
+                                className='form-control'
+                                type='text'
+                                onChange={this.handleChange}
+                                value={this.state.surname}>
+                            </input>
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Family name
+                            </label>
+                          <input
+                             name="familyName"
+                             className='form-control'
+                             type='text'
+                             onChange={this.handleChange}
+                             value={this.state.familyName}>
+                            </input>
+                       </div>
+                        <div className='form-group'>
+                            <label>
+                             Date of birth
+                            </label>
+                            <input
+                                name='dateOfBirth'
+                                className='form-control'
+                                type='date'
+                                onChange={this.handleChange}
+                                value={this.state.dateOfBirth}>
+                            </input>
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Birthplace
+                            </label>
+                            <input
+                                name='birthplace'
+                                className='form-control'
+                                type='text'
+                                onChange={this.handleChange}
+                                value={this.state.birthplace}>
+                            </input>
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Nationality
+                            </label>
+                            <input
+                                name='nationality'
+                                className='form-control'
+                                type='text'
+                                onChange={this.handleChange}
+                                value={this.state.nationality}>
+                            </input>
+                        </div>    
+                        <div className='form-group'>
+                            <label>
+                                Gender
+                            </label>
+                            <select name='gender'
+                                className='form-control'
+                                value={this.state.gender}
+                                onChange={this.handleChange}>
+                                    <option value='male'>
+                                        Male
+                                    </option>
+                                    <option value='female'>
+                                        Female
+                                    </option>
+                            </select>
+                        </div>
+                        <div className='caption'>
+                            Parents data
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Mother's name
+                            </label>
+                            <input
+                                name='mothersName'
+                                className='form-control'
+                                type='text'
+                                onChange={this.handleChange}
+                                value={this.state.mothersName}>
+                            </input>
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Father's name
+                            </label>
+                            <input
+                                name='fathersName'
+                                className='form-control'
+                                type='text'
+                                onChange={this.handleChange}
+                                value={this.state.fathersName}>
+                            </input>
+                        </div>
+                        <div className='form-group'>
+                            <label>
+                                Mother's maiden name
+                            </label>
+                            <input
+                                name='maidenName'
+                                className='form-control' 
+                                type='text' 
+                                onChange={this.handleChange}
+                                value={this.state.maidenName}/>
+                        </div>
+                    </form>
                 </div>
             </div>
         )
