7c26f6c17bc116d9cdf12ce83788fdf7bf885156
Jola <recka.jolanta@gmail.com>
2018-08-29 02:17:25 +0200

    declaration confirm button added

--- src/components/applicationForm/declaration/Declaration.js
+++ src/components/applicationForm/declaration/Declaration.js
@@ -9,6 +9,7 @@ class Declaration extends React.Component{
             showModal: 'block',
             disabled: true,
         }
+        this.confirmBtn = document.querySelector('input'); 
     }
     handleChange = event =>{
         event.target.checked ? this.setState({
@@ -25,28 +26,43 @@ class Declaration extends React.Component{
     
     
     render(){
+     
         return(
         <div className='declaration-background'
             style={{display: this.state.showModal}}>
         <div className='declaration container'>
         <DeclarationText/>
+        {/* <form className='container'>
+            <div className='form-check'>
         <input
+            className='form-check-input' 
             type='checkbox'
             checked={this.state.checked} 
             onChange={this.handleChange}
         />
-        <label>I confirm.</label>
+        <label className='form-check-label'>Confirm</label> */}
+        <div className="btn-group-toggle container" data-toggle="buttons">
+  <label className="btn btn-secondary">
+    <input type="checkbox" 
+    autoComplete="off" 
+    onChange={this.handleChange}/>{!this.state.disabled? <span><i className="far fa-check-circle"></i></span> : <span>Confirm</span>}
+    </label>
+
         <button className='btn btn-secondary closing-button'
                 style={{display: this.state.showModal}}
                 disabled={this.state.disabled}
-                onClick={this.handleClick}>
+                onClick={this.handleClick}
+                >
             {/* <i class="fas fa-pen-fancy"></i> */}
-            <i class="fas fa-file-signature"></i>
+            <i className="fas fa-file-signature"></i>
             &nbsp;
             Fill out the form
         </button>
         </div>
+        {/* </form>*/}
+        </div> 
         </div>
+
         )}
 }
 export default Declaration;
